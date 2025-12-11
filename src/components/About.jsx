3072c4b24813ef07843d8e518036260f0c874e3f
chathurangaFromUovt <chathurangaFromUovt@gmail.com>
2025-12-12 01:41:00 +0530

    colour-fixed-v2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
                     Hi, I’m <strong>Chathuranga™</strong>, a student at <strong>University of Vocational Technology Sri Lanka</strong>, pursuing a B.Tech degree in Multimedia & WEB.
                 </p>
                 <p className="text-gray-700 text-lg leading-relaxed mb-6 text-center">
-                    I am passionate about full-stack development, web technologies, and deploying real-world projects. I love experimenting with new technologies like Docker, CI/CD, and cloud deployment.
+                    I am passionate about Multimedia developments, web technologies, and deploying real-world projects. I love experimenting with new technologies like Docker, CI/CD, and cloud deployment.
                 </p>
                 <p className="text-gray-700 text-lg leading-relaxed text-center">
                     I am continuously learning, building, and sharing my work. My goal is to create meaningful software that is useful and accessible to everyone.
